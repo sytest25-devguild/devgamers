fec0d67378799f211339362dc2672c0e2ddde32d
gabriel note <gabriel.emil.note@gmail.com>
2026-06-03 13:04:36 +0200

    in progress
    Not finished, testing around

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import setupListeners from "./components/clientListeners";
+import { useEffect } from "react";
+import testButtonEmitMessage from "./components/testButtonEmitMessage";
+import { getSocket, disconnectSocket } from "./components/socketStuff/connectSocket";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("useEffect in Home ran");
+    getSocket();
+  }, []);
+  
+
+  
+  const handleButtonClick = () => {
+    testButtonEmitMessage();
+  }
+  
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -10,7 +27,7 @@ export default function Home() {
         <p className="mt-3 text-2xl text-center sm:text-left">
           This is the landing page of the game. 
         </p>
-        <button className="mt-5 px-5 py-3 rounded-lg bg-blue-500 text-white text-lg hover:bg-blue-600">
+        <button onClick={handleButtonClick} className="mt-5 px-5 py-3 rounded-lg bg-blue-500 text-white text-lg hover:bg-blue-600">
           testbutton
           </button>
       </main>
